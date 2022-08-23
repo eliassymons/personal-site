68784ca6784e766b639b7b79b07faebba4a22ecf
eliassymons <111077574+eliassymons@users.noreply.github.com>
2022-08-23 10:14:44 -0700

    configure for deployment

---
+++ Desktop/hosting-site/src/components/About.js
@@ -0,0 +1,22 @@
+export default function About() {
+  return (
+    <div className="about-container" id="about">
+      <h3>about me</h3>
+      <div className="about-info">
+        <p>
+          Since graduating with a degree in Piano Performance and Composition in
+          2015, I've taught piano and scored music for commercials, short films,
+          runway shows, and feature films. Last year, I decided it was time for
+          a change. I wanted to pursue a career that was challenging and allowed
+          me to express my creativity. Fast forward 8 months and I've learned
+          the fundamentals of full stack development
+        </p>
+        <p>
+          HTML, CSS, JavaScript, React.js, and Express.js have been the focus of
+          my studies. I feel confident that my skills will make me an asset as a
+          fullstack or front-end developer.
+        </p>
+      </div>
+    </div>
+  );
+}
