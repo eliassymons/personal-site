0e03f2fd8f6f6a9a0533226929348863ad64b939
Elias Symons <eisymonsmusic@gmail.com>
2022-08-24 10:18:25 -0700

    added links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,29 +5,40 @@ import park from "../images/pexels-rudolf-jakkel-418831.jpg";
 export default function Projects() {
   return (
     <div className="projects-container" id="projects">
-      <div className="project" onClick="location.href='https://eliassymons.github.io/time-signatures';">
+      <div className="project" >
         <div className="project-info">
           <h3>time signatures</h3>
+          
           <p>
             A game that tests a musician's ability to identify the meter of
             various music.
           </p>
+          <div className="links"><a href="https://eliassymons.github.io/time-signatures" className="link">visit site</a>
+          <a href="https://github.com/eliassymons/time-signatures.git" className="link">view code</a></div>
+          
         </div>
         <img className="project-img" alt="time-signature-site" src={time} />
       </div>
-      <div className="project" onClick="location.href='https://eliassymons.github.io/cutting-board-site';">
+      <div className="project" >
         <div className="project-info">
           <h3>woodshop</h3>
           <p>A sample website for an artisan woodshop.</p>
+        <div className="links"><a href="https://eliassymons.github.io/cutting-board-site" className="link">visit site</a>
+        <a href="https://github.com/eliassymons/cutting-board-site.git" className="link">view code</a></div>
+          
         </div>
         <img className="project-img" alt="cutting-board-site" src={board} />
       </div>
-      <div className="project" onClick="location.href='https://eliassymons.github.io/national-parks';">
+      <div className="project" >
         <div className="project-info">
           <h3>national parks</h3>
           <p>
             An application that allows the user to retrieve basic information and images for all national parks and historic sites located in the US.
           </p>
+          <div className="links">
+          <a href="https://eliassymons.github.io/national-parks" className="link">visit site</a>
+        <a href="https://github.com/eliassymons/national-parks.git" className="link">view code</a>
+          </div>
         </div>
         <img alt="natl-park-site" className="project-img" src={park} />
       </div>
